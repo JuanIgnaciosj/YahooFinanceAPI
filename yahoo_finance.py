f053b893526894b1427ced9d4586839ee469282d
Juan Ignacio <juanignacio.saavedrajeria@gmail.com>
2024-07-27 11:44:05 -0400

    Notebookv2

--- yahoo_finance.py
+++ yahoo_finance.py
@@ -1,20 +1,24 @@
 """
     ESTE SCRIPT ES UNA IMPLEMENTACION SIMPLE DE LA API ALOJADA EN yahoofinance para python.
 
-    
+
     ##cambiar todo lo que esta de aqui para abajo pero tomar como ejemplo.
-    LA IMPLEMENTACION ACTUAL CONSISTE EN CONSULTAS SIMPLES QUE SERAN EJECUTADAS POR UN BOT DE TELEGRAM
+    EL SCRIPT SIGUIENTE CONSIDERA DIFERENTES OPERACIONES QUE SERAN UTILIZADAS EN MI PROCESOS DE VALORACION DE EMPRESAS, EL SCRIPT PRINCIPAL CON EL USO DE LA LIBRERIA SE PUEDE CONSULTAR EN https://pypi.org/project/yfinance/
+    Y TAMBIEN EN LA PAGINA DEL CREADOR. https://aroussi.com/post/python-yahoo-finance.
+
+
+    LA IMPLEMENTACION ACTUAL CONSISTE EN CONSULTAS SIMPLES SOBRE LA INFORMACION HISTORICA RELEVANTE DE UNA ACCION EN BASE A SU SIMBOLO O CODIGO CORRESPONDIENTE INSCRITO EN LA NYSE.
     -- EN DESARROLLO --
-    LA IMPLEMENTACION CONSIDERA 3 FUNCIONES GENERALES:
-        - CONSULTA DEL VALOR ACTUAL DE LOS INDICADORES DISPONIBLES, SEGUN LA WEB DE https://mindicador.cl/
-        - CONSULTA EL VALOR DE LOS ULTIMOS 30 DIAS.
+    LA IMPLEMENTACION CONSIDERA LAS SIGUIENTES FUNCIONES:
+        - OBTIENE TODA LA INFORMACION BASICA QUE SE TIENE REGISTRO EN https://finance.yahoo.com/ .
+        - OBTENER EL ULTIMO MES DE COMPORTAMIENTO DEL ACTIVO EN BASE A SU SIMBOLO ORIGINAL.
         - CONSULTA EL VALOR DE LOS INDICADORES PARA UNA FECHA EN ESPECIFICO. TAMBIEN LA FUNCION SE ENCUENTRA EN DISPONIBLE PARA CONSULTAR CON UN SOLO INDICADOR.
         - CONSULTA EL VALOR DE UN INDICADOR PARA UN AÑO EN ESPECIFICO.
         - LOS INDICADORES DISPONIBLES SON: ["uf", "ivp", "dolar", "dolar_intercambio", "euro", "ipc","utm", "imacec", "tpm", "libra_cobre", "tasa_desempleo", "bitcoin"]. PARA MAS DETALLE CONSULTAR mindicador.cl
         - TODAS LAS FUNCIONES RETORNAN UN OBJETO DE TIPO DICCIONARIO QUE PERMITE FACILMENTE TRABAJAR CON EL (ESTE SE PENSO PARA TRABAJAR CON PANDAS Y SEABORN)
     Returns:
         _type_: _description_
-    """
+"""
 
 import yfinance as yf
 import pandas as pd
@@ -27,50 +31,63 @@ msft = yf.Ticker("MSFT")  # Indica la accion a consultar.
 # get all stock info (LA API VIENE COMO UN DICCIONARIO):
 
 
-# def ObtenerInformacion(ticker):
-#     ticker = yf.Ticker(ticker)
-#     for keys, values in ticker.info.items():
-#         print(str(keys) + ": " + str(values))
+def ObtenerInformacion(ticker):
+    ticker = yf.Ticker(ticker)
+    for keys, values in ticker.info.items():
+        # Imprime en el formato necesario la informacion de los principales ejecutivos
+        if str(keys) == 'companyOfficers':
+            print('companyOfficers:')
+           /** OJO: AQUI VIENE UNA LISTA CON LOS EJECUTIVOS PRINCIPALES ***/
+            for exec in range(0, len(ticker.info["companyOfficers"])):
+                for keys1, values1 in ticker.info["companyOfficers"][exec].items():
+                    print("         "+str(keys1) + ": " + str(values1))
+                print("\n")
+        # imprime el diccionario normalmente
+        else:
+            print(str(keys) + ": " + str(values))
 
 
 # ObtenerInformacion(ticker=ticker)
 
 # get historical market data
 
-
-# def ObtenerUltimoMes(ticker, number): ##PARA TENER EN CONSIDERACION ['1d', '5d', '1mo', '3mo', '6mo', '1y', '2y', '5y', '10y', 'ytd', 'max']
-#     ticker_ob = yf.Ticker(ticker)
-#     per = str(number) + 'mo'
-#     # Revisar que otras opciones tengo
-#     hist = ticker_ob.history(period=str(per))
-#     # Se puede hacer un grafico para ver como se ha comportado
-#     df_hist = pd.DataFrame(data=hist)
-#     # print(df_hist)
-#     # Genera visualizacion
-#     sns.lineplot(df_hist, x=df_hist.index, y=df_hist['Close'])
-#     # Format
-#     plt.suptitle("Valor de " + ticker +
-#                  " en dólares (Cierre sesión). Ult mes")
-#     plt.ylabel("Valor de " + ticker + " en dólares (Cierre sesión).")
-#     plt.xlabel("Valor de " + ticker + " por fecha.")
-#     # Show
-#     plt.show()
+def ObtenerUltimoMes(ticker, number): ##PARA TENER EN CONSIDERACION ['1d', '5d', '1mo', '3mo', '6mo', '1y', '2y', '5y', '10y', 'ytd', 'max']
+    ticker_ob = yf.Ticker(ticker)
+    per = str(number) + 'mo'
+    # Revisar que otras opciones tengo
+    hist = ticker_ob.history(period=str(per))
+    # Se puede hacer un grafico para ver como se ha comportado
+    df_hist = pd.DataFrame(data=hist)
+    # print(df_hist)
+    # Genera visualizacion
+    sns.lineplot(df_hist, x=df_hist.index, y=df_hist['Close'])
+    # Format
+    plt.suptitle("Valor de " + ticker +
+                 " en dólares (Cierre sesión). Ult mes")
+    plt.ylabel("Valor de " + ticker + " en dólares (Cierre sesión).")
+    plt.xlabel("Valor de " + ticker + " por fecha.")
+    # Show
+    plt.show()
 
 # ObtenerUltimoMes(ticker, 6)
 
 
 # # show meta information about the history (requires history() to be called first)
-msft.history_metadata
+# msft.history_metadata # Indica cuando se actualiza
 
-# hist = msft.history(period="6m")
-
-# print(msft.history(period="6m").history_metadata)
 
 # # show actions (dividends, splits, capital gains)
-# msft.actions
+actions = pd.DataFrame(data=msft.actions)
+print(actions)
 # msft.dividends
+dividends = pd.DataFrame(data=msft.dividends)
+print(dividends)
 # msft.splits
+splits = pd.DataFrame(data=msft.splits)
+print(splits)
 # msft.capital_gains  # only for mutual funds & etfs
+# cap_gains = pd.DataFrame(data=msft.capital_gains)
+# print(cap_gains)
 
 # # show share count
 # msft.get_shares_full(start="2022-01-01", end=None)
